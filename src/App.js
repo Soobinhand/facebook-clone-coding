f154642981c2ae3647d5121fcc13aaf7f33ab712
soobinhand <chelsea9528@naver.com>
2021-07-10 02:43:52 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's build facebook clone</h1>
+      <Header></Header>
     </div>
   );
 }
