e666d63df3a6da77f75d55649a097439789b5955
soobinhand <chelsea9528@naver.com>
2021-07-14 16:45:22 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Feed from './Feed';
 import Header from './Header.js';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+
+      <div className="app__body">
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
